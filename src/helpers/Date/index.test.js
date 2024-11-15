1c1ee2fadc9074c5a39721a611b6bcdb07c9e81d
Noreyni Sery <69noreyni.sery@gmail.com>
2024-11-15 10:44:35 +0100

    Add id to section in order to navigate through sections when clicking on link in navbar

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -4,7 +4,7 @@ describe("Date helper", () => {
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
             const date = new Date("2022-01-01");
-            expect(getMonth(date)).toEqual('janvier');
+            expect(getMonth(date)).toEqual('w');
         });
         it("the function return juillet for 2022-07-08 as date", () => {
             const date = new Date("2022-07-08");
