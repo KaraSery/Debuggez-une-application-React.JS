1d9edfc1e1405239c4e7a7283c0d30417131f080
Noreyni Sery <69noreyni.sery@gmail.com>
2024-11-15 11:10:06 +0100

    Fix Typo error at Date helper test

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -4,7 +4,7 @@ describe("Date helper", () => {
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
             const date = new Date("2022-01-01");
-            expect(getMonth(date)).toEqual('w');
+            expect(getMonth(date)).toEqual('janvier');
         });
         it("the function return juillet for 2022-07-08 as date", () => {
             const date = new Date("2022-07-08");
@@ -12,4 +12,3 @@ describe("Date helper", () => {
         });
     });
 })
-
